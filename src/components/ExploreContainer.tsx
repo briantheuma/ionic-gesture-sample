aa327cb4addbcce81c5053bef48c018b58aa1268
brian theuma <theumabrian@gmail.com>
2020-03-04 15:55:30 +0100

    changed drag target container mishap

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,17 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { createGesture, Gesture } from '@ionic/react';
 import './ExploreContainer.css';
 
 interface ContainerProps {
   name: string;
 }
 
-const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
-  return (
-    <div className="container">
-      <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
-  );
+//const ExploreContainer: React.FC<ContainerProps> = ({ name }) =>
+class ExploreContainer extends Component<ContainerProps> {
+
+  
+  componentDidMount() {
+    this.gesture.enable();
+  }
+
+
+  pg:any = document.querySelector('.ppp');
+  gesture: Gesture = createGesture({
+    el: document.querySelector('.container')!,
+    threshold: 15,
+    gestureName: 'my-gest',
+    onMove: (detail) => {
+      console.log('test');
+      //this.onMove(detail)
+    }
+  });
+
+  onMove = (detail: any) => {
+    console.log('IN IN IN MOVE!');
+    const type = detail.type;
+    const currentX = detail.currentX;
+    const deltaX = detail.deltaX;
+    const velocityX = detail.velocityX;
+    //let pg = document.querySelector('p');
+    this.pg.innerHTML = `
+    <div>Type: ${type}</div>
+    <div>Current X: ${currentX}</div>
+    <div>Delta X: ${deltaX}</div>
+    <div>Velocity X: ${velocityX}</div>
+  `
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <strong>{this.props.name}</strong>
+        <p>click and drag anywhere on this page</p>
+        <p className="ppp"></p>
+      </div>
+    );
+  }
+
 };
 
 export default ExploreContainer;
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
-const Tab1: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <IonHeader collapse="condense">
+class Tab1 extends Component {
+  render() {
+    return (
+      <IonPage>
+        <IonHeader>
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle>Drag Test</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-      </IonContent>
-    </IonPage>
-  );
+        <IonContent>
+          <IonHeader collapse="condense">
+            <IonToolbar>
+              <IonTitle size="large">Drag test</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          <ExploreContainer name="drag test" />
+        </IonContent>
+      </IonPage>
+    );
+  }
 };
 
 export default Tab1;
